fa994a7056f86a2a0cc56433032b0aa24737312b
heeryoung <winterbooks@naver.com>
2024-04-30 00:31:12 +0900

    refactor: CurrencyCard 위젯에 order 프로퍼티 추가

--- lib/widgets/currency_card.dart
+++ lib/widgets/currency_card.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class CurrencyCard extends StatelessWidget {
   final String name, amount, code;
   final IconData icon;
+  final double order;
   final bool isInverted;
 
   // private variable
@@ -14,77 +15,80 @@ class CurrencyCard extends StatelessWidget {
     required this.amount,
     required this.code,
     required this.icon,
-    required this.isInverted,
-  });
+    required this.order,
+  }) : isInverted = order % 2 == 0;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      // 컨테이너 밖으로 넘치는 부분(overflow) 잘라내기
-      clipBehavior: Clip.hardEdge,
-      decoration: BoxDecoration(
-        color: isInverted ? Colors.white : _blackColor,
-        borderRadius: BorderRadius.circular(20),
-      ),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 25,
-          vertical: 25,
+    return Transform.translate(
+      offset: Offset(0, -18 * (order - 1)),
+      child: Container(
+        // 컨테이너 밖으로 넘치는 부분(overflow) 잘라내기
+        clipBehavior: Clip.hardEdge,
+        decoration: BoxDecoration(
+          color: isInverted ? Colors.white : _blackColor,
+          borderRadius: BorderRadius.circular(20),
         ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  name,
-                  style: TextStyle(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 25,
+            vertical: 25,
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    name,
+                    style: TextStyle(
+                      color: isInverted ? _blackColor : Colors.white,
+                      fontSize: 26,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        amount,
+                        style: TextStyle(
+                          color: isInverted ? _blackColor : Colors.white,
+                          fontSize: 18,
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 5,
+                      ),
+                      Text(
+                        code,
+                        style: TextStyle(
+                          color: isInverted ? _blackColor : Colors.white,
+                          fontSize: 18,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+              // 부모 위젯에 영향 주지 않고 크기 키우기
+              Transform.scale(
+                scale: 2,
+                // 위젯 x, y좌표 지정
+                child: Transform.translate(
+                  offset: const Offset(-2, 12),
+                  child: Icon(
+                    icon,
                     color: isInverted ? _blackColor : Colors.white,
-                    fontSize: 26,
-                    fontWeight: FontWeight.w600,
+                    size: 70,
                   ),
                 ),
-                const SizedBox(
-                  height: 15,
-                ),
-                Row(
-                  children: [
-                    Text(
-                      amount,
-                      style: TextStyle(
-                        color: isInverted ? _blackColor : Colors.white,
-                        fontSize: 18,
-                      ),
-                    ),
-                    const SizedBox(
-                      width: 5,
-                    ),
-                    Text(
-                      code,
-                      style: TextStyle(
-                        color: isInverted ? _blackColor : Colors.white,
-                        fontSize: 18,
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-            // 부모 위젯에 영향 주지 않고 크기 키우기
-            Transform.scale(
-              scale: 2,
-              // 위젯 x, y좌표 지정
-              child: Transform.translate(
-                offset: const Offset(-2, 12),
-                child: Icon(
-                  icon,
-                  color: isInverted ? _blackColor : Colors.white,
-                  size: 70,
-                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
